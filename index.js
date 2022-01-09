d87895657d44be5d881a66afd52b0f4e48902657
Amir Velasquez <amir.velasquez1@gmail.com>
2022-01-08 21:33:56 -0400

    BugFix

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.on('ready', () => {
 	console.log('Logged in as ' + client.user.tag + '!');
 });
+client.on('message', async (interaction) => {
+    console.log("message recieved")
+	if (!interaction.isCommand()) return;
 
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
 client.login(process.env.DISCORD_TOKEN);
 
