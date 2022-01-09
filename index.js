561c20db128c49161eafcaa4d246d9c2b088f96e
Amir Velasquez <amir.velasquez1@gmail.com>
2022-01-08 23:15:47 -0400

    Herku Node version solved p2

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ client.on('ready', () => {
 	console.log('Logged in as ' + client.user.tag + '!');
 });
 client.on('message', async (interaction) => {
-    console.log("message recieved")
+	console.log('message recieved');
 	if (!interaction.isCommand()) return;
-
+	if (interaction.content === 'ping') {
+		await interaction.reply('Pong!');
+	}
 	if (interaction.commandName === 'ping') {
 		await interaction.reply('Pong!');
 	}
 });
 client.login(process.env.DISCORD_TOKEN);
-
